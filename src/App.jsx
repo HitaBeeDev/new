333f9a1dac0a876dbedba4b7e6d9e0625a2c5dab
HitaBeeDev <anahita.amiri103@gmail.com>
2025-01-30 14:41:33 +0100

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
-import { createBrowserRouter } from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
 import Home from "./ui/Home";
 import Menu from "./features/menu/Menu";
+import Cart from "./features/cart/Cart";
+import CreateOrder from "./features/order/CreateOrder";
+import Order from "./features/order/Order";
 
-createBrowserRouter([
+const router = createBrowserRouter([
   {
     path: "/",
     element: <Home />,
@@ -12,10 +15,22 @@ createBrowserRouter([
     path: "/menu",
     element: <Menu />,
   },
+  {
+    path: "/cart",
+    element: <Cart />,
+  },
+  {
+    path: "/order/new",
+    element: <CreateOrder />,
+  },
+  {
+    path: "/order/:orderId",
+    element: <Order />,
+  },
 ]);
 
 function App() {
-  return <div>hello</div>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
