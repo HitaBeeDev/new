e7c6158f6ec66baacbd38fa47308410977a7ec2a
HitaBeeDev <anahita.amiri103@gmail.com>
2025-01-31 11:37:49 +0100

    ui

---
+++ src/ui/Header.jsx
@@ -0,0 +1,13 @@
+import { Link } from "react-router-dom";
+
+function Header() {
+  return (
+    <div>
+      <Link to="/">Home logo food</Link>
+
+      <p>anahita</p>
+    </div>
+  );
+}
+
+export default Header;
