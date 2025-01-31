5c7d86fa949b1c487120bd295668a2045c0f7c73
HitaBeeDev <anahita.amiri103@gmail.com>
2025-01-31 15:43:02 +0100

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import "./index.css";
-import Home from "./ui/Home";
-import Menu, { loader as menuLoader } from "./features/menu/Menu";
 import Cart from "./features/cart/Cart";
+import Menu, { loader as menuLoader } from "./features/menu/Menu";
 import CreateOrder from "./features/order/CreateOrder";
 import Order from "./features/order/Order";
+import "./index.css";
 import AppLayout from "./ui/AppLayout";
+import Error from "./ui/Error";
+import Home from "./ui/Home";
 
 const router = createBrowserRouter([
   {
     element: <AppLayout />,
+
+    errorElement: <Error />,
+
     children: [
       {
         path: "/",
@@ -19,6 +23,7 @@ const router = createBrowserRouter([
         path: "/menu",
         element: <Menu />,
         loader: menuLoader,
+        errorElement: <Error />,
       },
       {
         path: "/cart",
--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,9 +1,12 @@
 import { Link } from "react-router-dom";
+import SearchOrder from "../features/order/SearchOrder";
 
 function Header() {
   return (
     <div>
       <Link to="/">Home logo food</Link>
+
+      <SearchOrder />
 
       <p>anahita</p>
     </div>
