c84dfeb91b4f261905a2aee7e5d64c8cca758dbd
HitaBeeDev <anahita.amiri103@gmail.com>
2025-02-01 12:00:45 +0100

    ui

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,15 +1,17 @@
 import { Link } from "react-router-dom";
 import SearchOrder from "../features/order/SearchOrder";
+import Username from "../features/user/Username";
 
 function Header() {
   return (
-    <div>
-      <Link to="/">Home logo food</Link>
+    <header className="flex items-center justify-between border-b border-stone-200 bg-yellow-400 px-4 py-3 uppercase sm:px-6">
+      <Link to="/" className="tracking-widest">
+        Fast React Pizza Co.
+      </Link>
 
       <SearchOrder />
-
-      <p>anahita</p>
-    </div>
+      <Username />
+    </header>
   );
 }
 
