9e8ab74a5440735d1c8df63c8163abebbf50034d
HitaBeeDev <anahita.amiri103@gmail.com>
2025-02-04 19:56:41 +0100

    commit

--- src/features/user/userSlice.js
+++ src/features/user/userSlice.js
@@ -1,3 +1,15 @@
+const { createSlice } = require("@reduxjs/toolkit");
+
 const initialState = {
   username: "",
 };
+
+const userSlice = createSlice({
+  name: "user",
+  initialState,
+  reducers: {
+    updateName(state, action) {
+      state.username = action.payload;
+    },
+  },
+});
