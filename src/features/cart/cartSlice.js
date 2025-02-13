320f1201e2db74710bc7f731ae806a5965e2424f
HitaBeeDev <anahita.amiri103@gmail.com>
2025-02-13 12:08:02 +0100

    commit

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -18,10 +18,24 @@ const cartSlice = createSlice({
   name: "cart",
   initialState,
   reducers: {
-    addItem(state, action) {},
-    deleteItem(state, action) {},
-    increaseItemQuanity(state, action) {},
-    decreaseItemQuanity(state, action) {},
+    addItem(state, action) {
+      state.cart.push(action.payload);
+    },
+    deleteItem(state, action) {
+      state.cart = state.cart.filter((item) => item.pizzaId !== action.payload);
+    },
+    increaseItemQuanity(state, action) {
+      const item = state.cart.find((item) => item.pizzaId === action.payload);
+
+      item.quantity++;
+      item.totalPrice = item.unitPrice * item.quantity;
+    },
+    decreaseItemQuanity(state, action) {
+      const item = state.cart.find((item) => item.pizzaId === action.payload);
+
+      item.quantity--;
+      item.totalPrice = item.unitPrice * item.quantity;
+    },
     clearCart(state) {},
   },
 });
