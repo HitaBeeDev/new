e823082f453ffc6249913ae07cf1e7b345bdb063
HitaBeeDev <anahita.amiri103@gmail.com>
2025-02-19 21:11:45 +0100

    commit

--- src/Components/features/order/CreateOrder.jsx
+++ src/Components/features/order/CreateOrder.jsx
@@ -9,7 +9,7 @@ import store from "../../../store";
 import { formatCurrency } from "../../utils/helpers";
 import { fetchAddress } from "../user/userSlice";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faLocationDot } from "@fortawesome/free-solid-svg-icons";
+import { faBell, faLocationDot } from "@fortawesome/free-solid-svg-icons";
 
 // https://uibakery.io/regex-library/phone-number
 const isValidPhone = (str) =>
@@ -60,54 +60,69 @@ function CreateOrder() {
         method="POST"
         className="mt-6 border border-[#FFFBF5] rounded-[1.5rem] shadow-md grid grid-cols-2 items-center p-5"
       >
-        <div className="col-span-1 flex flex-col items-start justify-start">
-          <label className="font-['Quicksand'] font-medium text-[0.9rem]">
+        <div className="col-span-1 flex flex-col items-start justify-between h-full">
+          <label className="font-['Quicksand'] font-medium text-[0.9rem] text-[#5A4034] mt-5">
             First Name
           </label>
 
-          <label className="font-['Quicksand'] font-medium text-[0.9rem] mt-5">
+          <label className="font-['Quicksand'] font-medium text-[0.9rem]  text-[#5A4034]">
             Phone number
           </label>
 
-          <label className="font-['Quicksand'] font-medium text-[0.9rem] mt-5">
+          <label className="font-['Quicksand'] font-medium text-[0.9rem] mb-3 text-[#5A4034]">
             Address
           </label>
         </div>
 
         <div className="col-span-1 flex flex-col items-start justify-start">
-          <input
-            className="border border-[#F6E6DA] rounded-[1rem] font-['Quicksand'] font-medium text-[0.9rem] h-[1.8rem] w-[15rem]"
-            type="text"
-            name="customer"
-            defaultValue={username}
-            required
-          />
-
-          <input
-            className="border border-[#F6E6DA] rounded-[1rem] font-['Quicksand'] font-medium text-[0.9rem] h-[1.8rem] w-[15rem] mt-5"
-            type="tel"
-            name="phone"
-            required
-          />
-          {formErrors?.phone && (
-            <p className="text-[#2e1f1a] font-[500] text-xs">
-              {formErrors.phone}
-            </p>
-          )}
-
-          <div className="mt-5">
+          <div className="">
             <input
-              className="border border-[#F6E6DA] rounded-[1rem] font-['Quicksand'] font-medium text-[0.9rem] h-[1.8rem] w-[15rem]"
+              className="border border-[#F6E6DA] rounded-[1rem] text-[#5A4034]
+            font-['Quicksand'] font-medium text-[0.85rem] h-[2.5rem] w-[18rem] pl-3 focus:outline-none focus:border-[#F6E6DA]"
+              type="text"
+              name="customer"
+              defaultValue={username}
+              required
+              placeholder="Enter your first name"
+            />
+          </div>
+
+          <div>
+            <input
+              className="border border-[#F6E6DA] rounded-[1rem] font-['Quicksand'] font-medium 
+              text-[0.85rem] h-[2.5rem] w-[18rem] mt-7 pl-3 text-[#5A4034] focus:outline-none focus:border-[#F6E6DA]"
+              type="tel"
+              name="phone"
+              required
+              placeholder="Enter your phone number"
+            />
+
+            {formErrors?.phone && (
+              <div className="flex flex-row items-center mt-2">
+                <FontAwesomeIcon className="text-[#E8B4B8] w-7" icon={faBell} />
+
+                <p className="text-[#2e1f1a] font-semibold text-xs">
+                  Please provide a valid phone number for contact.
+                </p>
+              </div>
+            )}
+          </div>
+
+          <div className="mt-7 border border-[#F6E6DA] rounded-[1rem] font-['Quicksand'] font-normal h-[7rem] w-[18rem] relative pl-3">
+            <input
+              className="font-['Quicksand'] font-normal text-[0.8rem] h-[2.5rem] w-[18rem]"
               type="text"
               name="address"
               disabled={isLoadingAddress}
               defaultValue={address}
               required
+              placeholder="Enter your address"
             />
             {addressStatus === "error" && <p>{errorAddress}</p>}
 
             {!position.latitude && !position.longitude && (
               <button
+                className="absolute cursor-pointer right-1 top-1"
                 disabled={isLoadingAddress}
                 type="small"
                 onClick={(e) => {
@@ -115,14 +130,17 @@ function CreateOrder() {
                   dispatch(fetchAddress());
                 }}
               >
-                <FontAwesomeIcon icon={faLocationDot} />
+                <FontAwesomeIcon
+                  className="text-[#5A4034] w-9"
+                  icon={faLocationDot}
+                />
               </button>
             )}
           </div>
         </div>
 
-        <div>
-          <div className="flex flex-row gap-3 items-end">
+        <div className="mt-10">
+          <div className="flex flex-row gap-2 items-center">
             <input
               type="checkbox"
               name="priority"
@@ -132,15 +150,16 @@ function CreateOrder() {
             />
 
             <label
-              className="font-['Quicksand'] font-medium text-[0.9rem]"
+              className="font-['Quicksand'] font-semibold text-[0.9rem] text-[#5A4034]"
               htmlFor="priority"
             >
-              Want to yo give your order priority?
+              Want to prioritize your order?
             </label>
           </div>
 
-          <div className="flex flex-row gap-3 items-end bg-red-400">
+          <div className="mt-5">
             <input type="hidden" name="cart" value={JSON.stringify(cart)} />
+
             <input
               type="hidden"
               name="position"
@@ -151,7 +170,10 @@ function CreateOrder() {
               }
             />
 
-            <button disabled={isSubmitting || isLoadingAddress}>
+            <button
+              className="cursor-pointer"
+              disabled={isSubmitting || isLoadingAddress}
+            >
               {isSubmitting
                 ? "Placing order...."
                 : `Order now from ${formatCurrency(totalPrice)}`}
